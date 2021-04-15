43949db9eae1b6e889090e8340b7d670201f526d
JasonZFHuang <jasonhuangzf@gmail.com>
2021-04-15 12:38:24 -0700

    added door opening

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Switch bathDoor;
+
+    void Awake()
+    {
+        if (instance != null)
+            GameObject.Destroy(instance);
+        else
+            instance = this;
+
+        //DontDestroyOnLoad(this);
+    }
+
+    //handle interaction events
+    public void Interaction(string interaction)
+    {
+        // too lazy to make enums for this tbh
+        switch (interaction){
+            case "test":
+                print("test");
+                break;
+
+            case "bathroomDoor":
+                bathDoor.Change(bathDoor.item[1]);
+                SoundManager.PlayOneClip(AudioClips.singleton.openDoor, 1);
+
+                break;
+
+            default:
+                print("Invalid interaction string");
+                break;
+            
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public List<GameObject> item;
+    public GameObject active;
+
+    void Awake() {
+        ActivateObject();
+    }
+
+    //Choose
+    public void Change(GameObject obj) {
+        active = obj;
+        ActivateObject();
+    }
+    
+    void ActivateObject() {
+        foreach(GameObject obj in item) {
+            if (obj == active) {
+                obj.SetActive(true);
+            }
+            else {
+                obj.SetActive(false);
+            }
+        }
+    }
+}
